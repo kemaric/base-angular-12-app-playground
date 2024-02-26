5c0f8eb7c05ea72f9ea73f1ab25bb3a247196530
kemaric <kemaric@gmail.com>
2024-02-26 07:06:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# base-angular-12-app-playground
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kemaric/base-angular-12-app-playground)
---
+++ src/app/pokemon/pokemon-service.ts
@@ -0,0 +1,104 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+// import { Http, Headers, Response } from '@angular/http';
+
+import { Pokemon } from './pokemon.model';
+
+@Injectable({ providedIn: 'root' })
+export class PokemonService {
+  constructor(private http: HttpClient) {}
+
+  // Don't use Promises
+  // list() {
+  //   let headers = new Headers();
+  //   headers.append('Content-Type', 'application/json');
+
+  //   return this.http
+  //     .get('https://pokeapi.co/api/v2/pokedex/2/', {
+  //       headers: headers,
+  //     })
+  //     .pipe(map(res: Response) => {
+  //       let data = res.json();
+  //       let allPokemon = [];
+
+  //       data.pokemon_entries.forEach((entry) => {
+  //         let pokemon = new Pokemon();
+  //         pokemon.name = entry.pokemon_species.name;
+  //         pokemon.id = entry.entry_number;
+  //         allPokemon.push(pokemon);
+  //       });
+
+  //       return allPokemon;
+  //     })
+  //     .catch(this.handleError);
+  // }
+
+  /**
+   *
+   *
+   * Use Observables instead!
+   */
+  list$(): Observable<Pokemon[]> {
+    const HTTP_OPTIONS = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+    return this.http
+      .get('https://pokeapi.co/api/v2/pokedex/2/', HTTP_OPTIONS)
+      .pipe(
+        map((data: any) => {
+          let allPokemon = [];
+          data.pokemon_entries.forEach((entry) => {
+            let pokemon = new Pokemon();
+            pokemon.name = entry.pokemon_species.name;
+            pokemon.id = entry.entry_number;
+            allPokemon.push(pokemon);
+          });
+          return allPokemon;
+        }),
+        shareReplay()
+      );
+  }
+
+  get(id: number) {
+    const HTTP_OPTIONS = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+    return this.http
+      .get('https://pokeapi.co/api/v2/pokemon/' + id + '/', HTTP_OPTIONS)
+      .pipe(
+        map((data: any) => {
+          let pokemon = new Pokemon();
+          pokemon.name = data.name;
+          pokemon.id = data.id;
+
+          data.types.forEach((eachType) => {
+            pokemon.types.push(eachType.type.name);
+          });
+
+          data.stats.forEach((eachStat) => {
+            pokemon.stats.push({
+              name: eachStat.stat.name,
+              value: eachStat.base_stat,
+            });
+          });
+
+          return pokemon;
+        })
+      );
+  }
+
+  // private handleError(error: Response | any) {
+  //   let errMsg: string;
+  //   if (error instanceof Response) {
+  //     const body = error.json() || '';
+  //     const err = body.error || JSON.stringify(body);
+  //     errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+  //   } else {
+  //     errMsg = error.message ? error.message : error.toString();
+  //   }
+  //   console.error(errMsg);
+  //   return Promise.reject(errMsg);
+  // }
+}
